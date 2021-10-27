f8b5c1b636188a79d8cd6c9de334103153795e3a
Geoffrey <62641790+Beubine@users.noreply.github.com>
2021-10-27 15:01:23 +0200

    aled6

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
-Player1 = Player(id, name, pos_x, pos_y, health)
-Player2 = Player(id, name, pos_x, pos_y, health)
+Player1 = Player(id, name, position_x, position_y, health)
+Player2 = Player(id, name, position_x, position_y, health)
 Player.id = MQTT.add_id(player, id)
 initCombat(Player1, Player2)
 closeCombat(health, timer)
