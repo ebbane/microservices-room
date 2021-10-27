c84e26ffde25b4c80e7f9e7874adaf72f92754e3
Geoffrey <62641790+Beubine@users.noreply.github.com>
2021-10-27 14:20:40 +0200

    aled2

---
+++ Combat.py
@@ -0,0 +1,55 @@
+import time
+
+from Mqtt import MQTT
+
+class Combat :
+        def__init__(self, id_combat, init_taille_x, init_taille_y, seconds)
+
+
+        self.id_combat = int
+        self.init_player = int
+        self.init_player_position = int
+        self.init_taille_x = 400
+        self.init_taille_y = 1000
+        self.seconds = 90
+        self.score = int
+
+        def timer(seconds):
+                 print("Chrono : %ds" % seconde)
+                 for i in range(seconds): time.sleep(1)
+                 print("Fin")
+
+
+
+
+        def initCombat(self, Player1, Player2, timer):
+
+                init_taille_x
+                init_taille_y
+                timer(seconds)
+
+
+
+        def closeCombat(self, health, timer):
+                if Player.health == 0 and timer > 0 :
+                        self.score = 0
+                else :
+                        self.score = 1
+
+                if timer == 0 :
+                        self.score = 0
+
+                if not player.initmovement(key) :
+                        time.sleep(10)
+                        destroy(Combat)
+
+                destroy(Combat)
+
+
+Player1 = Player(id, name, pos_x, pos_y, health)
+Player2 = Player(id, name, pos_x, pos_y, health)
+Player.id = MQTT.add_id(player, id)
+initCombat(Player1, Player2)
+closeCombat(health, timer)
+
+#choisir les variables en tant voulues
--- Player.py
+++ Player.py
@@ -1,106 +1,48 @@
-import arcade
-from PIL.ImagePalette import random
+class Player :
+    def__init__(self, id, name, pos_x, pos_y, health)
 
-class Player: 
-    def __init__(self, x, y, speed, limit_l, limit_r, id_player):
-       
-        self.id = id_player
-        self.name = name
-
-        self.center_x = x
-        self.center_y = y
-        self.move_x = 0
-        self.move_y = 0
-        self.speed = speed
-
-        self.score = 0
-        self.lives = 3
-        self.blink_timer = 0
-
-        self.freeze = False
-        self.invicibility = False
-
-        self.aimlock = False
-        self.aiming_timer = 0
-        self.aiming_x = 0
-        self.aiming_y = 0
-        self.aiming_rand = 0
+    self.id = 0
+    self.name = string
+    self.health = 100
+    self.pos_x = int
+    self.pos_y = 50
+    self.backdash = int
+    self.attack = bool
+    self.guard = bool
 
 
-        self.limit_left = limit_l
-        self.limit_right = limit_r
 
+    def initmovement(self, key):  #function key_pressed
+     if "q" in key:
+            self.pos_x -=25
 
-    def set_key(self, up_key, down_key, left_key, right_key, aimlock_key, reload_key, shoot_key):
-        self.up_key = up_key
-        self.down_key = down_key
-        self.left_key = left_key
-        self.right_key = right_key
-        self.aimlock_key = aimlock_key
-        self.reload_key = reload_key
-        self.shoot_key = shoot_key
+     if "d" in key:
+            self.pos_x +=25
+    
+     if "s" in key:
+            self.back +=50
 
-   
-    def lose_one_live(self, player):
-        if not self.invicibility:
-            self.lives -= 1
-            if self.lives < 1:
-                self.freeze = True
-                self.invicibility = True
-                self.cur_fall_texture = 1
-                player.score += 3
-            else:
-                self.blink_timer = 1
-                player.score += 1
+     if "k" in key:
+            self.attack == true
 
+     if "l" in key:
+            self.guard == true
+     
+     return key
 
-    def key_pressed(self, key):
-        self.timer_inactivity = 0.0
+    def hitbox (self, player):      #set la hitbox
 
-        if key == self.aimlock_key and self.cur_throw_texture == 0:
-            self.aimlock = True
-            self.aiming_rand = random() * pi * 2
-        if key == self.shoot_key and self.cur_throw_texture == 0 and not self.freeze and self.snowball_count > 0:
-            self.shoot()
-            self.aimlock = False
-            self.cur_throw_texture = 1
-            self.aiming_timer = 0
-        if key == self.reload_key:
-            self.reload = True
-
-    def key_released(self, key):
-        self.timer_inactivity = 0.0
-
-        if key == self.up_key:
-            self.move_y -= self.speed
-        if key == self.down_key:
-            self.move_y += self.speed
-        if key == self.left_key:
-            self.move_x += self.speed
-        if key == self.right_key:
-            self.move_x -= self.speed
-        if key == self.aimlock_key and self.aimlock:
-            self.aimlock = False
-            self.aiming_timer = 0
-        if key == self.reload_key:
-            self.reload = False
-
-    def joystick(self, key):
-        self.timer_inactivity = 0.0
-
-        if "C" in key:
-            self.move_y = 0
-            self.move_x = 0
-        if "N" in key:
-            self.move_y = self.speed
-        if "S" in key:
-            self.move_y = -self.speed
-
-        if "E" in key:
-            self.move_x = self.speed
-        if "W" in key:
-            self.move_x = -self.speed
+     pos_x += 20
+     pos_y += 50
 
     
+    def hit(self): 
 
+     if self.attack == True and hitbox.pos_x1 <= hitbox.pos_x2 and self.guard == False:
+         health -=25
+         time.sleep(1);  #frame d'invulnerabilité
 
+    def guard(self):
+         for i in range(1):
+             self.guard == True
+             time.sleep(3) #permet de monter la garde toutes les 3s
