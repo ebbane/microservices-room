c853a8e84c48e036d6703c31aa105027cce3fd57
Geoffrey <62641790+Beubine@users.noreply.github.com>
2021-10-28 11:25:47 +0200

    aled12

--- game.py
+++ game.py
@@ -1,13 +1,14 @@
 import Combat
 import Player
-import Mqtt
 
-if __name__ =="__main__":
+def main():
+
+     
+    game = Combat()
+
     
-    Player1 = Player(id, name, position_x, position_y)
-    Player2 = Player(id, name, position_x, position_y)
-    Player.id = MQTT.add_id(player, id)
-    initCombat(Player1.position_x, Player1.position_y, Player2.position_x, Player2.position_y)
-    loop_afk()
-    closeCombat(health, timer)
-    destroy(Combat)
+    game.launch_timer()
+
+
+if __name__ == "main":
+       main()
