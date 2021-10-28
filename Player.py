7f6fbe692c91a92e200a6d48a05f682671c47cc1
Geoffrey <62641790+Beubine@users.noreply.github.com>
2021-10-28 11:20:16 +0200

    aled11

--- Player.py
+++ Player.py
@@ -1,48 +1,56 @@
+import time
+import asyncio
+from datetime import datetime
+
 class Player :
-    def __init__(self, id, name, pos_x, pos_y, score):
-       self.id = 0
-       self.name = ""
-       self.health = 100
-       self.position_x = pos_x
-       self.position_y = pos_y
-       self.attack = False
-       self.guard = False
-       self.score = score
+    def __init__(self, id, name, pos_x, pos_y, size_x, last_action):
+
+        self.id = -1
+        self.name = ""
+        self.health = 100
+        self.position_x = 310
+        self.position_y = 400
+        self.attack = False
+        self.guard = False
+        self.size_x = 120
+        self.last_action = 0
 
 
+        def initmovement(self, key):  #function key_pressed
+             self.last_action = datetime.now()
+             if "q" in key:
+                    self.position_x -=50
 
-    def initmovement(self, key):  #function key_pressed
-     if "q" in key:
-            self.position_x -=50
-
-     if "d" in key:
-            self.position_x +=25
+             if "d" in key:
+                    self.position_x +=25
     
-     if "s" in key:
-            self.position_x -=25
+             if "s" in key:
+                    self.position_x -=25
 
-     if "k" in key:
-            self.attack == true
+             if "k" in key:
+                    self.attack = true
 
-     if "l" in key:
-            self.guard == true
+             if "l" in key:
+                    self.guard = true
+
+             if self.position_x > self.arena_size:
+                    self.position_x = self.arena_size
+
+             if self.position_x <= other.position_x :
+                 self.position_x -= 20
+                    
      
-     return key
+             return key
 
-    def hitbox (self, player):      #set la hitbox
+        def hitbox(self, player):      #set la hitbox
 
-     pos_x += 20
-     pos_y += 50
+            return self.size_x + self.position_x
 
-    def hit(self, other): 
-
-     if self.attack == True and self.hitbox.pos_x <= other.hitbox.pos_x and other.guard == False:
-         other.health -=25
-        # to do frame invulnerabiblité
-
-
-
-    def guard(self):
-         for i in range(1):
-             self.guard == True
-           # to do
+        async def hit(self, other):
+            
+             self.last_action = datetime.now()
+             self.attack = True
+             if self.hitbox.pos_x <= other.hitbox.pos_x and not other.guard and not self.guard:
+                  other.health -=25
+                  await asyncio.sleep(1)
+                  self.attack = False
