2bcc476ab4f74cacd8463383bebeaf68f8585a77
ebbane <diet.ebbane@gmail.com>
2021-10-28 11:22:06 +0200

    set mqtt subscribe

--- Room.py
+++ Room.py
@@ -2,27 +2,30 @@ import logging
 import time
 from mqttClient import * 
 
-
-
 class Room():
     def __init__(self):
         pass
     
-    def player_connexion(self, msg):
-        logging.info(f'Received message : {msg}')
-        # msg.payload = 
-            # msg.get('key')
+    def on_player_connexion(self, msg):
+        print (msg)
+        
+        # print(f'Received message : {msg}')
+            
+            # msg.get('joueur1').get('nom')
+            # msg.get('joueur1').get('id')
             # Création des joueur a partir des id
         
-    def player1_action(self, msg):
-        logging.info(f'Received message : {msg}')
+    def on_player1_action(self, msg):
         # Apelle fonction déplacement 
         # set player = player1
+        print (msg)
+        control = msg.get('control')
         
-    def player2_action(self, msg):
-        logging.info(f'Received message : {msg}')
+    def on_player2_action(self, msg):
+        print(msg)        
+        # player = player2
+        control = msg.get('control')
         # Apelle fonction déplacement 
-        # set player = player2
        
             
     
@@ -31,9 +34,9 @@ class Room():
         sig = signal('message')
         sig.send({'topic': topic, 'body': payload})
         
-    def position(self):
-        position2= {'player1_x': 150, 'player1_y': 200, 'player2_x': 100, 'player2_y': 150}
-        self.send_topic('game/match', position2)
+    # def position(self):
+    #     position2= {'player1_x': 15, 'player1_y': 200, 'player2_x': 100, 'player2_y': 150}
+    #     self.send_topic('game/match', position2)
                 
     
             
@@ -45,35 +48,41 @@ def main():
 
     # ---------------------------------
     # Initializing MQTT
-    mqtt_client = MQTTClient(['room/connexion', 'room/joueur1', 'room/joueur2' ])
+    mqtt_client = MQTTClient(['prediction/infos', 'room/player1', 'room/player2' ])
     mqtt_client.setup()
     mqtt_client.run()
 
     # ---------------------------------
     # TODO: Start my application
     app = Room()
-    app.position()
     
-    def listen_topic(self):
+    
+    def listen_topic():
+        # print('coucou')
         
         def on_message_room_connexion(msg):
-            app.player_connexion(msg)        
+            app.on_player_connexion(msg)        
     
-        sig = signal('room/connexion')
+        sig = signal('prediction/infos')
         sig.connect(on_message_room_connexion)
         
         def on_message_player1(msg):
-            app.player1_action(msg)        
+            app.on_player1_action(msg)        
     
-        sig = signal('room/joueur1')
+        sig = signal('room/player1')
         sig.connect(on_message_player1)
         
         def on_message_player2(msg):
-            app.player2_action(msg)        
+            app.on_player2_action(msg)        
     
-        sig = signal('room/joueur2')
+        sig = signal('room/player2')
         sig.connect(on_message_player2)
         
+    listen_topic()
+    while True :
+        pass
+    # mqtt_client.wait() 
+
 
 
     # ---------------------------------
